bebf0b3b920fcf1bc574f432035506f8cae54e53
Christopher Smith <22749214+CryptoChrisJames@users.noreply.github.com>
2026-03-14 01:09:52 -0700

    chore: removing everything ansible

--- src/embedded.rs
+++ src/embedded.rs
@@ -7,19 +7,11 @@ pub const PACK_FRONTEND_VALUES_TEMPLATE: &str = include_str!("embedded/templates
 pub const PACK_BACKEND_VALUES_TEMPLATE: &str = include_str!("embedded/templates/pack/values.backend.yaml");
 pub const PACK_FULLSTACK_VALUES_TEMPLATE: &str = include_str!("embedded/templates/pack/values.fullstack.yaml");
 
-// Templates
+// Base Build Templates
 pub const BIND9_DB: &str = include_str!("embedded/templates/base_build/bind9.db.j2");
 pub const NAMED_CONF_LOCAL: &str = include_str!("embedded/templates/base_build/named.conf.local.j2");
 pub const NAMED_CONF_OPTIONS: &str = include_str!("embedded/templates/base_build/named.conf.options.j2");
 pub const REGISTRIES: &str = include_str!("embedded/templates/base_build/registries.yaml.j2");
-
-// Playbooks
-pub const BUILD_CONTROL_PLANE: &str = include_str!("embedded/playbooks/base_build/build_control_plane.yml");
-pub const BUILD_NODES: &str = include_str!("embedded/playbooks/base_build/build_nodes.yml");
-pub const POST_BUILD: &str = include_str!("embedded/playbooks/base_build/post_build.yml");
-pub const VALIDATE_CLUSTER: &str = include_str!("embedded/playbooks/base_build/validate_cluster.yml");
-pub const DESTROY_CONTROL_PLANE: &str = include_str!("embedded/playbooks/base_destroy/destroy_control_plane.yml");
-pub const DESTROY_NODES: &str = include_str!("embedded/playbooks/base_destroy/destroy_nodes.yml");
 
 // Helm charts
 pub const NYA_CHART: &str = include_str!("embedded/helm/Chart.yaml");
@@ -28,27 +20,5 @@ pub const NYA_BACKEND_TEMPLATE: &str = include_str!("embedded/helm/backend.yaml"
 pub const NYA_FRONTEND_TEMPLATE: &str = include_str!("embedded/helm/frontend.yaml");
 
 //MetalLB
-pub const METALLB_IP_POOL: &str = include_str!("embedded/playbooks/base_build/metallb-ipaddresspool.yaml");
-pub const METALLB_L2ADV: &str = include_str!("embedded/playbooks/base_build/metallb-l2advertisement.yaml");
-
-pub fn get_playbook(name: &str) -> Option<&'static str> {
-    match name {
-        "build_control_plane" => Some(BUILD_CONTROL_PLANE),
-        "build_nodes" => Some(BUILD_NODES),
-        "post_build" => Some(POST_BUILD),
-        "validate_cluster" => Some(VALIDATE_CLUSTER),
-        "destroy_control_plane" => Some(DESTROY_CONTROL_PLANE),
-        "destroy_nodes" => Some(DESTROY_NODES),
-        _ => None,
-    }
-}
-
-pub fn get_base_template(name: &str) -> Option<(&'static str, &'static str)> {
-    match name {
-        "bind9_db" => Some(("bind9.db.j2", BIND9_DB)),
-        "conf_local" => Some(("named.conf.local.j2", NAMED_CONF_LOCAL)),
-        "conf_options" => Some(("named.conf.options.j2", NAMED_CONF_OPTIONS)),
-        "registries" => Some(("registries.yaml.j2", REGISTRIES)),
-        _ => None,
-    }
-}
+pub const METALLB_IP_POOL: &str = include_str!("embedded/templates/base_build/metallb/metallb-ipaddresspool.yaml");
+pub const METALLB_L2ADV: &str = include_str!("embedded/templates/base_build/metallb/metallb-l2advertisement.yaml");
